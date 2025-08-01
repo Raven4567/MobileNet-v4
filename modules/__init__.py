06fbf1434ba45c1c7f5086fa5478268702fab1e3
Raven4567 <aidencaldwell42@gmail.com>
2025-08-01 16:47:41 +0300

    Fix gross error.

--- modules/__init__.py
+++ modules/__init__.py
@@ -7,11 +7,3 @@ from .IB import IB # noqa: F401
 from .FFN import FFN # noqa: F401
 
 from .MobileMQA import MobileMQA # noqa: F401
-
-'''
-Optional block that there's no in original architecture
-but that you can add in your custom architectures.
-'''
-
-from .SE import SE  # noqa: E402, F401
-from .ResNeXt import ResNeXt # noqa: E402, F401
